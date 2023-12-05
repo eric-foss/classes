0f08de42a6fc49f9ed32fa98989fa141c651c254
eric-foss <ericadamfoss@gmail.com>
2023-12-04 16:23:46 -0800

    huh

--- MAE170/Lab 6/AveragedNonlinearPlots.m
+++ MAE170/Lab 6/AveragedNonlinearPlots.m
@@ -1,4 +1,4 @@
-close all; clear all; clc;
+ close all; clear all; clc;
 
 Tamb = 22;
 
@@ -40,30 +40,44 @@ T(1, :) = T1(1:minlen);
 T(2, :) = T2(1:minlen);
 T(3, :) = T3(1:minlen);
 
+theta(1, :) = (T1(1:minlen)-T1_amb(1:minlen))./(T1(1)-T1_amb(1:minlen));
+theta(2, :) = (T2(1:minlen)-T2_amb(1:minlen))./(T2(1)-T2_amb(1:minlen));
+theta(3, :) = (T3(1:minlen)-T3_amb(1:minlen))./(T3(1)-T3_amb(1:minlen));
+
+avgTheta_AcyFree = mean(theta);
+
 avgT_AcyFree = mean(T);
+
 clear T;
+clear theta;
 
 %% Acrylic Forced
 
 load('lab6AcrylicForced1.mat'); clc;
 
 plateT1 = tempPlateC;
+ambT1 = tempAmbC;
 
 load('lab6AcrylicForced2.mat'); clc;
 
 plateT2 = tempPlateC;
+ambT2 = tempAmbC;
 
 load('lab6AcrylicForced3.mat'); clc;
 
 plateT3 = tempPlateC;
+ambT3 = tempAmbC;
 
 [max1, ind1] = max(plateT1);
 [max2, ind2] = max(plateT2);
 [max3, ind3] = max(plateT3);
 
 T1 = plateT1(ind1: end);
+T1_amb = ambT1(ind1:end);
 T2 = plateT2(ind2: end);
+T2_amb = ambT2(ind2:end);
 T3 = plateT3(ind3: end);
+T3_amb = ambT3(ind3:end);
 
 len(1) = length(T1);
 len(2) = length(T2);
@@ -75,30 +89,43 @@ T(1, :) = T1(1:minlen);
 T(2, :) = T2(1:minlen);
 T(3, :) = T3(1:minlen);
 
+theta(1, :) = (T1(1:minlen)-T1_amb(1:minlen))./(T1(1)-T1_amb(1:minlen));
+theta(2, :) = (T2(1:minlen)-T2_amb(1:minlen))./(T2(1)-T2_amb(1:minlen));
+theta(3, :) = (T3(1:minlen)-T3_amb(1:minlen))./(T3(1)-T3_amb(1:minlen));
+
+avgTheta_AcyForced = mean(theta);
+
 avgT_AcyForced = mean(T);
 clear T;
+clear theta;
 
 %% Metal Free
 
 load('lab6MetalFree1.mat'); clc;
 
 plateT1 = tempPlateC;
+ambT1 = tempAmbC;
 
 load('lab6MetalFree2.mat'); clc;
 
 plateT2 = tempPlateC;
+ambT2 = tempAmbC;
 
 load('lab6MetalFree3.mat'); clc;
 
 plateT3 = tempPlateC;
+ambT3 = tempAmbC;
 
 [max1, ind1] = max(plateT1);
 [max2, ind2] = max(plateT2);
 [max3, ind3] = max(plateT3);
 
 T1 = plateT1(ind1: end);
+T1_amb = ambT1(ind1:end);
 T2 = plateT2(ind2: end);
+T2_amb = ambT2(ind2:end);
 T3 = plateT3(ind3: end);
+T3_amb = ambT3(ind3:end);
 
 len(1) = length(T1);
 len(2) = length(T2);
@@ -110,30 +137,48 @@ T(1, :) = T1(1:minlen);
 T(2, :) = T2(1:minlen);
 T(3, :) = T3(1:minlen);
 
+theta(1, :) = (T1(1:minlen)-T1_amb(1:minlen))./(T1(1)-T1_amb(1:minlen));
+theta(2, :) = (T2(1:minlen)-T2_amb(1:minlen))./(T2(1)-T2_amb(1:minlen));
+theta(3, :) = (T3(1:minlen)-T3_amb(1:minlen))./(T3(1)-T3_amb(1:minlen));
+
+avgTheta_MetalFree = mean(theta);
+
 avgT_MetalFree = mean(T);
 clear T;
+clear theta;
 
 %% MetalForced
 
 load('lab6MetalForced1.mat'); clc;
 
 plateT1 = tempPlateC;
+ambT1 = tempAmbC;
 
 load('lab6MetalForced2.mat'); clc;
 
 plateT2 = tempPlateC;
+ambT2 = tempAmbC;
 
 load('lab6MetalForced3.mat'); clc;
 
 plateT3 = tempPlateC;
+ambT3 = tempAmbC;
 
 [max1, ind1] = max(plateT1);
 [max2, ind2] = max(plateT2);
 [max3, ind3] = max(plateT3);
 
-T1 = plateT1(ind1+5: end);
-T2 = plateT2(ind2+5: end);
-T3 = plateT3(ind3+5: end);
+ind1 = ind1+7;
+ind2 = ind2+6;
+ind3 = ind3+6;
+
+
+T1 = plateT1(ind1: end);
+T1_amb = ambT1(ind1:end);
+T2 = plateT2(ind2: end);
+T2_amb = ambT2(ind2:end);
+T3 = plateT3(ind3: end);
+T3_amb = ambT3(ind3:end);
 
 len(1) = length(T1);
 len(2) = length(T2);
@@ -145,8 +190,15 @@ T(1, :) = T1(1:minlen);
 T(2, :) = T2(1:minlen);
 T(3, :) = T3(1:minlen);
 
+theta(1, :) = (T1(1:minlen)-T1_amb(1:minlen))./(T1(1)-T1_amb(1:minlen));
+theta(2, :) = (T2(1:minlen)-T2_amb(1:minlen))./(T2(1)-T2_amb(1:minlen));
+theta(3, :) = (T3(1:minlen)-T3_amb(1:minlen))./(T3(1)-T3_amb(1:minlen));
+
+avgTheta_MetalForced = mean(theta);
+
 avgT_MetalForced = mean(T);
 clear T;
+clear theta;
 
 
 %% Plot
@@ -165,17 +217,14 @@ legend('Acrylic Plate Temperature w/ Free Convection', ...
 xlabel('Time (s)'); ylabel('Temperature (C)');
 
     
-theta_AcyFree = (avgT_AcyFree-Tamb)/(avgT_AcyFree(1)-Tamb);
-theta_AcyForced = (avgT_AcyForced-Tamb)/(avgT_AcyForced(1)-Tamb);
-theta_MetalFree = (avgT_MetalFree-Tamb)/(avgT_MetalFree(1)-Tamb);
-theta_MetalForced = (avgT_MetalForced-Tamb)/(avgT_MetalForced(1)-Tamb);
+
 
 figure(2);
 
-y1 = log(theta_AcyFree);
-y2 = log(theta_AcyForced);
-y3 = log(theta_MetalFree);
-y4 = log(theta_MetalForced);
+y1 = log(avgTheta_AcyFree);
+y2 = log(avgTheta_AcyForced);
+y3 = log(avgTheta_MetalFree);
+y4 = log(avgTheta_MetalForced);
 
 x1 = 1:length(y1);
 x2 = 1:length(y2);
@@ -187,7 +236,7 @@ plot(y1, 'b--'); hold on;
 plot(y2, 'g-.'); hold on;
 plot(y3, 'r:'); hold on;
 plot(y4, 'k-'); hold on;
-xlabel('Time Constant'); ylabel('Ln(Theta)');
+xlabel('Time (s)'); ylabel('Ln(Theta)');
 legend('Acrylic Free', 'Acrylic Forced', 'Metal Free', 'Metal Forced');
 
 subplot(2, 1, 2);
@@ -217,5 +266,5 @@ plot(x1, f1(1)*x1 + f1(2),'b--'); hold on;
 plot(x2, f2(1)*x2 + f2(2), 'g-.'); hold on;
 plot(x3, f3(1)*x3 + f3(2), 'r:'); hold on;
 plot(x4, f4(1)*x4 + f4(2), 'k-'); hold on;
-xlabel('Time Constant'); ylabel('Ln(Theta)');
+xlabel('Time (s)'); ylabel('Ln(Theta)');
 legend('Acrylic Free', 'Acrylic Forced', 'Metal Free', 'Metal Forced');
