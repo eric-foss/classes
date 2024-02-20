7f69642c8602c3fd7d1736a0a05beef7c77520f6
eric-foss <ericadamfoss@gmail.com>
2024-02-20 10:22:11 -0800

    started mae182 hw4

---
+++ MAE182/HW4/A4_1.m
@@ -0,0 +1,17 @@
+%% MAE182 Assignment 4 Problem 1
+% Eric Foss
+% A17068006
+clear; close all; clc;
+
+%% Part A
+
+y = [1; 2; 1];
+
+R = [1/2 0 0;
+     0 1 0;
+     0 0 1];
+
+H = [1; 1; 1];
+
+xbar = 2;
+
