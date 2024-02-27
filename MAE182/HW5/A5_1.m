b2de154c8746c97cc4a2c852e71e68ac83437336
eric-foss <ericadamfoss@gmail.com>
2024-02-27 00:10:22 -0800

    mae182 hw5

--- MAE182/HW5/A5_1.m
+++ MAE182/HW5/A5_1.m
@@ -3,13 +3,8 @@
 % A17068006
 clear; close all; clc;
 
-%% Part A: Batch Algorithm
-
-
-
 % Initial State
-phi0 = eye(3);
-X0_ref = [1; 1; 1; reshape(phi0, 9, 1)];
+X0_ref = [1; 1; 1];
 
 % A Piori
 x0_bar = [0; 0; 0];
@@ -18,23 +13,59 @@ P0_bar = [4 0 0;
           0 0 1];
 
 % Measurements
-t = 0:1;
+t = 1;
 Y = 2;
 R = 1;
+H_tilde = [1 0 0];
 
-itr = 4;
-Lambda = inv(P0_bar);
-N = Lambda*x0_bar;
+%% Batch Algorithm
 
+%STM and State
+phi1 = stm(1);
+X1_ref = phi1*X0_ref;
 
-for j = 1:itr
-    
-    for t = 1:2
-        X_ref
+%H Matrix
+H1 = H_tilde*phi1;
 
+%Observation Deviation
+y1 = Y - X1_ref(1);
 
+%Initial State Deviation
+x0_hat = inv(H1'*inv(R)*H1 + inv(P0_bar))*(H1'*inv(R)*y1 + inv(P0_bar)*x0_bar);
 
+%Estimated Initial State
+X0_hat = X0_ref + x0_hat %%PART A ANSWER%%
+
+%% Sequential Algorithm
+
+%STM and State
+phi1 = stm(t);
+X1_ref = phi1*X0_ref;
+
+%Observation Deviation
+y1 = Y - X1_ref(1);
+
+%A Priori Reference Covariance
+P1_bar = phi1*P0_bar*phi1';
+
+%Kalman Gain
+K1 = P1_bar*H_tilde'*inv(H_tilde*P1_bar*H_tilde' + R);
+
+%SOMETHING WRONG SOMEWHERE
+X1_hat = X1_ref + K1*(y1 - H_tilde*X1_ref);
+
+X0_hat = inv(phi1)*X1_hat
+
+function phi = stm(t)
+%STM Computes state transition matrix from 0 to t for the dynamics in HW5
+%Problem 1
+
+    phi = [1 t 0.5*t^2;
+           0 1 t;
+           0 0 1];
 
 end
+
+
 
     
