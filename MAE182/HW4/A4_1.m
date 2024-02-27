9b3a87aeb7afc9fc5cc629b9e70503d796af979f
eric-foss <ericadamfoss@gmail.com>
2024-02-26 20:20:30 -0800

    comments on mae 182 hw4

--- MAE182/HW4/A4_1.m
+++ MAE182/HW4/A4_1.m
@@ -5,26 +5,27 @@ clear; close all; clc;
 
 %% Part A
 
+%Givens
 y = [1; 2; 1];
-
 R = [1/2 0 0;
      0 1 0;
      0 0 1];
-
 H = [1; 1; 1];
-
 xbar = 2;
-
 Pbar = 1/2;
 
+%Deviation
 xhat = inv(H'*inv(R)*H + inv(Pbar))*(H'*inv(R)*y + inv(Pbar)*xbar);
 
 %% Part B
 
+%Variance
 P = inv(H'*inv(R)*H + inv(Pbar));
 
+%Standard Deviation
 sigma = sqrt(P);
 
 %% Part C
 
+%Error
 ehat = y - H*xhat;
