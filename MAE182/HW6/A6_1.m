48b15cfcc8156481063f3e53f5665ac7fb846d8f
eric-foss <ericadamfoss@gmail.com>
2024-03-12 15:49:09 -0700

    added gitignore for asv thanks aldrin!

--- MAE182/HW6/A6_1.m
+++ MAE182/HW6/A6_1.m
@@ -19,12 +19,41 @@ C = [ 0  0  1  0  0];
 
 D = 0;
 
-syms gamma a b;
+x0 = [1; 2; 3; 4; 5];
+
+sys = ss(A, B, C, D);
+
+pzmap(sys);
+
+syms g a b;
 
 R(a, b) = [ a  0;
             0  b];
 
-Q = gamma;
+Q(g) = [ 0  0  0  0  0;
+         0  0  0  0  0;
+         0  0  g  0  0;
+         0  0  0  0  0;
+         0  0  0  0  0];
 
-H(gamma, a, b) = gamma*X(3)^2 + u'*R*u;
+
+%% CASE 1 %%%%%%%%%%%
+
+R1 = R(0.1, 0.1);
+Q1 = Q(3);
+[K, S, eig] = lqr(sys, Q1, R1);
+
+pzmap(eig);
+
+t = [0 10];
+[~, x] = ode45(@dynamics1, t, x0, odeset('RelTol',1e-12,'AbsTol',1e-15));
+
+
+
+function xdot = dynamics1(~, x)
+
+xdot = (A - B*K1)*x;
+
+end
+
 
