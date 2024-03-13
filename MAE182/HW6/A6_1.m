97f349397275aae792922bbeffe8ec30e250f49d
eric-foss <ericadamfoss@gmail.com>
2024-03-12 18:16:06 -0700

    182 hw6 problem 1 case 1

--- MAE182/HW6/A6_1.m
+++ MAE182/HW6/A6_1.m
@@ -2,6 +2,7 @@
 % Eric Foss
 % A17068006
 
+
 A = [ 5  0  3  0  1;
       3  0  0 -2  0;
       0 -2  4  1  0;
@@ -37,23 +38,44 @@ Q(g) = [ 0  0  0  0  0;
          0  0  0  0  0];
 
 
+%Dynamics
+time = [0 10];
+xdot = @(x, A, B, K) (A - B*K)*x;
+
+
 %% CASE 1 %%%%%%%%%%%
 
 R1 = R(0.1, 0.1);
 Q1 = Q(3);
-[K, S, eig] = lqr(sys, Q1, R1);
+[K1, S1, eig1] = lqr(sys, Q1, R1);
 
-pzmap(eig);
-
-t = [0 10];
-[~, x] = ode45(@dynamics1, t, x0, odeset('RelTol',1e-12,'AbsTol',1e-15));
+[t1, x1] = ode45(@(t, x) xdot(x, A, B, K1), time, x0, odeset('RelTol',1e-12,'AbsTol',1e-15));
 
 
 
-function xdot = dynamics1(~, x)
+figure(2); hold on;
+plot(t1, x1(:, 1), '-g');
+plot(t1, x1(:, 2), '-b');
+plot(t1, x1(:, 3), '-k');
+plot(t1, x1(:, 4), '-c');
+plot(t1, x1(:, 5), '-r');
+legend('x1', 'x2', 'x3', 'x4', 'x5');
 
-xdot = (A - B*K1)*x;
+%% CASE 2 %%%%%%%%%%%%%
 
-end
+R2 = R(0, 0);
+Q2 = Q1;
+[K2, ~, eig2] = lqr(sys, Q2, R2);
+
+[t2, x2] = ode45(@(t, x) xdot(x, A, B, K2), time, x0, odeset('RelTol',1e-12,'AbsTol',1e-15));
+
+figure(3); hold on;
+plot(t2, x2(:, 1), '-g');
+plot(t2, x2(:, 2), '-b');
+plot(t2, x2(:, 3), '-k');
+plot(t2, x2(:, 4), '-c');
+plot(t2, x2(:, 5), '-r');
+legend('x1', 'x2', 'x3', 'x4', 'x5');
+
 
 
